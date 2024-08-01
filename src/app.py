a2a532046b230d63253c70fa7fe8957bba3ff77b
biagodoy <beatrizgodoy381@gmail.com>
2024-08-01 17:51:33 -0300

    add(time.py) time.py vai guardar as informações essenciais de um time

--- src/app.py
+++ src/app.py
@@ -1,1 +1,4 @@
+#to do criar um dispositivo para cadastrar um novo time
+#criando array ou variavel e uma função para cadastrar um novo time
+
 print("Hello world")
---
+++ src/models/time.py
@@ -0,0 +1,2 @@
+#to do mapear principais caracteristicas de um time
+# to do criar as variaveis do time de todas as caracteristcas mapeadas
